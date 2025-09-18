955c395ef1ee8ce9f6e7c53947abb2a4e7825fb2
Naveed latif <naveedlatif526@gmail.com>
2025-09-18 14:25:40 -0700

    Shifted to MySQL: added database config and packages

--- Sql/utils/database.js
+++ Sql/utils/database.js
@@ -1,10 +1,11 @@
-const mysql=require('mysql2');
+const mysql = require("mysql2");
+require("dotenv").config();
 
-const pool=mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"root",
-    database:"airbnb",
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
-module.exports=pool.promise();
+module.exports = pool.promise();
